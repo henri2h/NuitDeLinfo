a9fd358eb4dd0e95d08679fbeac6134ac0385c51
Henri2h <henri.carnot@hotmail.fr>
2020-12-03 19:45:43 +0100

    start home page refactoring

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,71 @@
-import React from 'react'
-import { GetServerSideProps } from 'next'
-import Layout from '../components/Layout'
-import Post, { PostProps } from '../components/Post'
+import React from "react";
+import { GetServerSideProps } from "next";
+import Layout from "../components/Layout";
+import Post, { PostProps } from "../components/Post";
+import {Level, Heading, Section, Hero, Container} from "react-bulma-components";
 
 type Props = {
-  feed: PostProps[]
-}
+  feed: PostProps[];
+};
 
-const Home : React.FC<Props> = props => {
+const Home: React.FC<Props> = (props) => {
   return (
     <Layout>
       <div className="page">
-        <h1>My Blog</h1>
+      <Section>
+        <Hero >
+          <Hero.Body>
+            <Container>
+              <Heading>
+              Welcome back user
+              </Heading>
+              <Heading subtitle size={3}>
+              Subtitle
+              </Heading>
+            </Container>
+          </Hero.Body>
+        </Hero>
+      </Section>
         <main>
-        
+          <Level renderAs="nav">
+            <Level.Item textAlignment="centered">
+              <div>
+                <p className="heading">tweets</p>
+                <Heading renderAs="p">3,456</Heading>
+              </div>
+            </Level.Item>
+            <Level.Item textAlignment="centered">
+              <div>
+                <p className="heading">following</p>
+                <Heading renderAs="p">123</Heading>
+              </div>
+            </Level.Item>
+            <Level.Item textAlignment="centered">
+              <div>
+                <p className="heading">followers</p>
+                <Heading renderAs="p">456K</Heading>
+              </div>
+            </Level.Item>
+            <Level.Item textAlignment="centered">
+              <div>
+                <p className="heading">likes</p>
+                <Heading renderAs="p">789</Heading>
+              </div>
+            </Level.Item>
+          </Level>
         </main>
       </div>
     </Layout>
-  )
-}
+  );
+};
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  const res = await fetch('http://localhost:3000/api/feed')
+  const res = await fetch("http://localhost:3000/api/feed");
   console.log(res.text());
-  const feed = []
+  const feed = [];
   return {
     props: { feed },
-  }
-}
+  };
+};
 
-export default Home
+export default Home;
