af1e8f2d773e07482bdea6bd401af0328d8978ec
Henri2h <henri.carnot@hotmail.fr>
2020-12-03 19:45:30 +0100

    New header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,69 +1,31 @@
-import React from 'react'
-import Link from 'next/link'
-import { useRouter } from 'next/router'
+import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { Navbar } from "react-bulma-components";
 
 const Header: React.FC = () => {
-  const router = useRouter()
-  const isActive: (pathname: string) => boolean =
-    pathname => router.pathname === pathname
+  const router = useRouter();
+  const isActive: (pathname: string) => boolean = (pathname) =>
+    router.pathname === pathname;
 
-  return(
-    <nav>
-      <div className="left">
-        <Link href="/">
-          <a className="bold" data-active={isActive('/')}>
-            Blog
-          </a>
-        </Link>
-        <Link href="/drafts">
-          <a data-active={isActive('/drafts')}>Drafts</a>
-        </Link>
-      </div>
-      <div className="right">
-        <Link href="/signup">
-          <a data-active={isActive('/signup')}>Signup</a>
-        </Link>
-        <Link href="/create">
-          <a data-active={isActive('/create')}>+ Create draft</a>
-        </Link>
-      </div>
-      <style jsx>{`
-        nav {
-          display: flex;
-          padding: 2rem;
-          align-items: center;
-        }
-
-        .bold {
-          font-weight: bold;
-        }
-
-        a {
-          text-decoration: none;
-          color: #000;
-          display: inline-block;
-        }
-
-        .left a[data-active='true'] {
-          color: gray;
-        }
-
-        a + a {
-          margin-left: 1rem;
-        }
-
-        .right {
-          margin-left: auto;
-        }
-
-        .right a {
-          border: 1px solid black;
-          padding: 0.5rem 1rem;
-          border-radius: 3px;
-        }
-      `}</style>
-    </nav>
-  )
-}
-
-export default Header
+  return (
+    <Navbar fixed="top">
+      <Navbar.Brand>
+        <Navbar.Item renderAs="a" href="#">
+          Notre App
+        </Navbar.Item>
+        <Navbar.Burger />
+      </Navbar.Brand>
+      <Navbar.Menu>
+        <Navbar.Container>
+          <Navbar.Item href="#">Mes activités</Navbar.Item>
+        </Navbar.Container>
+        <Navbar.Container position="end">
+          <Navbar.Item href="#">Ajouter une activitée</Navbar.Item>
+          <Navbar.Item href="#">At the end</Navbar.Item>
+        </Navbar.Container>
+      </Navbar.Menu>
+    </Navbar>
+  );
+};
+export default Header;
