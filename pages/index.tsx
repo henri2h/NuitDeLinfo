df0acf9998107d0a292f072198bbf9fb84d97600
Henri2h <henri.carnot@hotmail.fr>
2020-12-04 00:16:04 +0100

    Hey

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,155 +13,105 @@ import {
   Box,
   Media,
   Image,
+  Button,
 } from "react-bulma-components";
+
+import CalendarHeatmap from "react-calendar-heatmap";
+import "react-calendar-heatmap/dist/styles.css";
 
 type Props = {
   feed: PostProps[];
 };
 
 const Home: React.FC<Props> = (props) => {
+  const HeroStyle = {
+    backgroundImage:
+      'url("https://worth1000.s3.amazonaws.com/submissions/501500/501884_d50d_1024x2000.jpg")',
+    backgroundSize: "cover",
+    backgroundPositionY: "center",
+  };
+
+  const titleStyle = {
+    color: "white",
+    fontSize: "9vw",
+  };
+  const subtitleStyle = {
+    color: "white",
+    fontSize: "4rem",
+  };
+
   return (
     <Layout>
       <div className="page">
-        <Section>
-          <Hero>
-            <Hero.Body>
-              <Container>
-                <Heading>Welcome back user</Heading>
-                <Heading subtitle size={3}>
-                  Subtitle
-                </Heading>
-              </Container>
-            </Hero.Body>
-          </Hero>
-        </Section>
+        <Hero size="fullheight" style={HeroStyle}>
+          <Hero.Body>
+            <Container>
+              <Heading size={1} style={titleStyle}>
+                Wesh je suis un surfeur
+              </Heading>
+              <Heading size={3} style={subtitleStyle} subtitle>
+                Casse casse casse
+              </Heading>
+            </Container>
+          </Hero.Body>
+        </Hero>
+
         <main>
           <Columns>
             <Columns.Column>
-              <Box>
-                <Media>
-                  <Media.Item renderAs="figure" position="left">
-                    <Image
-                      size={64}
-                      alt="64x64"
-                      src="http://bulma.io/images/placeholders/128x128.png"
-                    />
-                  </Media.Item>
-                  <Media.Item>
-                    <Content>
-                      <p>
-                        <strong>John Smith</strong> <small>@johnsmith</small>{" "}
-                        <small>31m</small>
-                        <br />
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Proin ornare magna eros, eu pellentesque tortor
-                        vestibulum ut. Maecenas non massa sem. Etiam finibus
-                        odio quis feugiat facilisis.
-                      </p>
-                    </Content>
-                  </Media.Item>
-                </Media>
-              </Box>
-            </Columns.Column>
-            <Columns.Column>
-              <Box>
-                <Media>
-                  <Media.Item renderAs="figure" position="left">
-                    <Image
-                      size={64}
-                      alt="64x64"
-                      src="http://bulma.io/images/placeholders/128x128.png"
-                    />
-                  </Media.Item>
-                  <Media.Item>
-                    <Content>
-                      <p>
-                        <strong>John Smith</strong> <small>@johnsmith</small>{" "}
-                        <small>31m</small>
-                        <br />
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Proin ornare magna eros, eu pellentesque tortor
-                        vestibulum ut. Maecenas non massa sem. Etiam finibus
-                        odio quis feugiat facilisis.
-                      </p>
-                    </Content>
-                  </Media.Item>
-                </Media>
-              </Box>
-            </Columns.Column>
+              <Section>
 
-            <Columns.Column>
-              <Box>
-              <p>
-                        <strong>John Smith</strong> <small>@johnsmith</small>{" "}
-                        <small>31m</small>
-                        <br />
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Proin ornare magna eros, eu pellentesque tortor
-                        vestibulum ut. Maecenas non massa sem. Etiam finibus
-                        odio quis feugiat facilisis.
-                      </p>
-              </Box>
+              <div>
+                  <Heading>Mes activités</Heading>
+                  <Section>
+                  <CalendarHeatmap
+                    startDate={new Date("2020-01-01")}
+                    endDate={new Date("2020-12-31")}
+                    values={[
+                      { date: "2020-01-01", count: 12 },
+                      { date: "2020-01-22", count: 122 },
+                      { date: "2020-09-30", count: 38 },
+                      { date: "2020-09-1", count: 1 },
+                      { date: "2020-10-28", count: 64 },
+                      { date: "2020-10-29", count: 50 },
+                      { date: "2020-10-30", count: 1 },
+                      // ...and so on
+                    ]}
+                  />
+                  </Section>
+                </div>
+            
+                <Box>
+                  <Heading>Ajouter vos activités</Heading>
+                  <Level renderAs="nav">
+                    <Level.Item textAlignment="centered">
+                      <div>
+                        <p>
+                          Hello beauté
+                          <br />
+                          Qu'es ce que tu a fais aujourd'hui ?
+                        </p>
+                      </div>
+                    </Level.Item>
+
+                    <Level.Item textAlignment="centered">
+                      <div>
+                        <Button renderAs="span" color="info">
+                          Ajouter mon activitée d'aujourd'hui
+                        </Button>
+                      </div>
+                    </Level.Item>
+                  </Level>
+                </Box>
+  </Section>
+            </Columns.Column>
+            <Columns.Column size={4}>
+              <Section>
+                <Image src="https://vignette3.wikia.nocookie.net/p__/images/a/a7/3641-brice-de-nice-3-un-tournage-pour-septembre.jpg/revision/latest?cb=20161025120448&path-prefix=protagonist" />
+              </Section>
             </Columns.Column>
           </Columns>
 
-          <Box
-            responsive={{
-              mobile: {
-                display: {
-                  value: "block",
-                },
-              },
-              tablet: {
-                display: {
-                  value: "flex",
-                },
-              },
-              desktop: {
-                display: {
-                  value: "inline-flex",
-                  only: true,
-                },
-              },
-              widescreen: {
-                display: {
-                  value: "inline-block",
-                },
-              },
-            }}
-            hide={{
-              tablet: {
-                hide: true,
-                only: true,
-              },
-              widescreen: {
-                hide: true,
-              },
-            }}
-          >
-            <Media>
-              <Media.Item renderAs="figure" position="left">
-                <Image
-                  size={64}
-                  alt="64x64"
-                  src="http://bulma.io/images/placeholders/128x128.png"
-                />
-              </Media.Item>
-              <Media.Item>
-                <Content>
-                  <p>
-                    <strong>John Smith</strong> <small>@johnsmith</small>{" "}
-                    <small>31m</small>
-                    <br />
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Proin ornare magna eros, eu pellentesque tortor vestibulum
-                    ut. Maecenas non massa sem. Etiam finibus odio quis feugiat
-                    facilisis.
-                  </p>
-                </Content>
-              </Media.Item>
-            </Media>
-          </Box>
           <Level renderAs="nav">
             <Level.Item textAlignment="centered">
               <div>
