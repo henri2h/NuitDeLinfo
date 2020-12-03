a10560bff6e2c2e7a5993a6dc8946b2d23659b8b
Henri2h <henri.carnot@hotmail.fr>
2020-12-03 20:03:38 +0100

    added boxes

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,18 @@ import React from "react";
 import { GetServerSideProps } from "next";
 import Layout from "../components/Layout";
 import Post, { PostProps } from "../components/Post";
-import {Level, Heading, Section, Hero, Container} from "react-bulma-components";
+import {
+  Level,
+  Heading,
+  Section,
+  Content,
+  Container,
+  Columns,
+  Hero,
+  Box,
+  Media,
+  Image,
+} from "react-bulma-components";
 
 type Props = {
   feed: PostProps[];
@@ -12,21 +23,145 @@ const Home: React.FC<Props> = (props) => {
   return (
     <Layout>
       <div className="page">
-      <Section>
-        <Hero >
-          <Hero.Body>
-            <Container>
-              <Heading>
-              Welcome back user
-              </Heading>
-              <Heading subtitle size={3}>
-              Subtitle
-              </Heading>
-            </Container>
-          </Hero.Body>
-        </Hero>
-      </Section>
+        <Section>
+          <Hero>
+            <Hero.Body>
+              <Container>
+                <Heading>Welcome back user</Heading>
+                <Heading subtitle size={3}>
+                  Subtitle
+                </Heading>
+              </Container>
+            </Hero.Body>
+          </Hero>
+        </Section>
         <main>
+          <Columns>
+            <Columns.Column>
+              <Box>
+                <Media>
+                  <Media.Item renderAs="figure" position="left">
+                    <Image
+                      size={64}
+                      alt="64x64"
+                      src="http://bulma.io/images/placeholders/128x128.png"
+                    />
+                  </Media.Item>
+                  <Media.Item>
+                    <Content>
+                      <p>
+                        <strong>John Smith</strong> <small>@johnsmith</small>{" "}
+                        <small>31m</small>
+                        <br />
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Proin ornare magna eros, eu pellentesque tortor
+                        vestibulum ut. Maecenas non massa sem. Etiam finibus
+                        odio quis feugiat facilisis.
+                      </p>
+                    </Content>
+                  </Media.Item>
+                </Media>
+              </Box>
+            </Columns.Column>
+            <Columns.Column>
+              <Box>
+                <Media>
+                  <Media.Item renderAs="figure" position="left">
+                    <Image
+                      size={64}
+                      alt="64x64"
+                      src="http://bulma.io/images/placeholders/128x128.png"
+                    />
+                  </Media.Item>
+                  <Media.Item>
+                    <Content>
+                      <p>
+                        <strong>John Smith</strong> <small>@johnsmith</small>{" "}
+                        <small>31m</small>
+                        <br />
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Proin ornare magna eros, eu pellentesque tortor
+                        vestibulum ut. Maecenas non massa sem. Etiam finibus
+                        odio quis feugiat facilisis.
+                      </p>
+                    </Content>
+                  </Media.Item>
+                </Media>
+              </Box>
+            </Columns.Column>
+
+            <Columns.Column>
+              <Box>
+              <p>
+                        <strong>John Smith</strong> <small>@johnsmith</small>{" "}
+                        <small>31m</small>
+                        <br />
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Proin ornare magna eros, eu pellentesque tortor
+                        vestibulum ut. Maecenas non massa sem. Etiam finibus
+                        odio quis feugiat facilisis.
+                      </p>
+              </Box>
+            </Columns.Column>
+          </Columns>
+
+          <Box
+            responsive={{
+              mobile: {
+                display: {
+                  value: "block",
+                },
+              },
+              tablet: {
+                display: {
+                  value: "flex",
+                },
+              },
+              desktop: {
+                display: {
+                  value: "inline-flex",
+                  only: true,
+                },
+              },
+              widescreen: {
+                display: {
+                  value: "inline-block",
+                },
+              },
+            }}
+            hide={{
+              tablet: {
+                hide: true,
+                only: true,
+              },
+              widescreen: {
+                hide: true,
+              },
+            }}
+          >
+            <Media>
+              <Media.Item renderAs="figure" position="left">
+                <Image
+                  size={64}
+                  alt="64x64"
+                  src="http://bulma.io/images/placeholders/128x128.png"
+                />
+              </Media.Item>
+              <Media.Item>
+                <Content>
+                  <p>
+                    <strong>John Smith</strong> <small>@johnsmith</small>{" "}
+                    <small>31m</small>
+                    <br />
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Proin ornare magna eros, eu pellentesque tortor vestibulum
+                    ut. Maecenas non massa sem. Etiam finibus odio quis feugiat
+                    facilisis.
+                  </p>
+                </Content>
+              </Media.Item>
+            </Media>
+          </Box>
           <Level renderAs="nav">
             <Level.Item textAlignment="centered">
               <div>
